4ceb187fd658011be514f387dcfe5bfe55c185fb
Saiiddd <muhzackysaid@gmail.com>
2024-12-11 12:08:18 +0700

    commit again

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 use App\Http\Controllers\UserController;
 
 Route::get('/login', [UserController::class, 'showLoginForm']);
@@ -9,3 +13,25 @@ Route::post('/login', [UserController::class, 'login']);
 
 Route::get('/register', [UserController::class, 'showRegisterForm']);
 Route::post('/register', [UserController::class, 'register']);
+
+// Sign In Page
+Route::get('/signin', function () {
+    return view('signin');
+})->name('signin');
+
+// Sign Up Page
+Route::get('/signup', function () {
+    return view('signup');
+})->name('signup');
+
+// Blog Page
+Route::get('/blog', function () {
+    return view('blog');
+})->name('blog');
+
+// Profile Page
+Route::get('/profile', function () {
+    return view('profile');
+})->name('profile');
+
+
